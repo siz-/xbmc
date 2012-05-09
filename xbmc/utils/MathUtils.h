12f894db2fb5933ede2531ac9d84cd3873166ad8
davilla <davilla@4pi.com>
2012-05-09 12:07:07 -0400

    [aml] disable asserts, some unknown call can assert and crash us

--- xbmc/utils/MathUtils.h
+++ xbmc/utils/MathUtils.h
@@ -48,8 +48,8 @@ namespace MathUtils
    */
   inline int round_int (double x)
   {
-    assert(x > static_cast<double>(INT_MIN / 2) - 1.0);
-    assert(x < static_cast <double>(INT_MAX / 2) + 1.0);
+    //assert(x > static_cast<double>(INT_MIN / 2) - 1.0);
+    //assert(x < static_cast <double>(INT_MAX / 2) + 1.0);
     const float round_to_nearest = 0.5f;
     int i;
     
@@ -125,8 +125,8 @@ namespace MathUtils
    */
   inline int truncate_int(double x)
   {
-    assert(x > static_cast<double>(INT_MIN / 2) - 1.0);
-    assert(x < static_cast <double>(INT_MAX / 2) + 1.0);
+    //assert(x > static_cast<double>(INT_MIN / 2) - 1.0);
+    //assert(x < static_cast <double>(INT_MAX / 2) + 1.0);
 
 #if !defined(__powerpc__) && !defined(__ppc__) && !defined(__arm__)
     const float round_towards_m_i = -0.5f;
